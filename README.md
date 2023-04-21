bae577903b414e74dd7793e8589985ce0faa5391
Mugamba669 <brunohectre@gmail.com>
2023-04-21 17:55:18 +0300

    Added  project documentation

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-# nyumbayo_app
+# Nyumbayo App
 
-A new Flutter project.
+An app that will help property owners or landlord to manage their tenants.
 
-## Getting Started
+## Landlord's UI Screenshots
+<div style="display:flex;flex-direction:row;justify-content:space-evenly;align-items:center;">
+<img src="./flutter_01.png" width="200"/>
+<img src="./flutter_02.png" width="200"/>
+<img src="./flutter_03.png" width="200"/>
+</div>
 
-This project is a starting point for a Flutter application.
+## Project Setup
+```
+    git clone https://github.com/bse23-4/nyumbayo_app.git
+```
+## Navigate to the project
+```
+cd nyumbayo_app
+```
+### Run the application
+```
+flutter run -v
+```
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Project Setup
+- Controllers
+- Models
+- Widgets
+- Views
+- Observers
+- Tools
+- Exports
+- Themes
+  
+## Tech Stack
+- Flutter (Dart)
+- Firebase
--- lib/tools/Utils.dart
+++ lib/tools/Utils.dart
@@ -1,48 +1,71 @@
 import 'dart:ui';
 
 import '/exports/exports.dart';
-void showOptions(BuildContext context){
-  showModalBottomSheet(
-            barrierColor: Colors.black.withOpacity(0.7),
-            backgroundColor: Colors.transparent,
-              context: context,
-              builder: (context) {
-                return BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-                  child: BottomSheet(
-                    backgroundColor: Colors.transparent,
-                      onClosing: () {},
-                      builder: (context) {
-                        return Container(
-                          height: MediaQuery.of(context).size.height,
-                          width: MediaQuery.of(context).size.width,
-                          color: Colors.transparent,
-                          child:Stack(
-                            children: [
-                              Column(
-                                children: [
-                                  ListTile(
-                                    onTap: (){
-                                      Navigator.pop(context);
-                                      Navigator.of(context).pushNamed(Routes.addTenant);
-                                    },
-                                    leading: const Icon(Icons.person_add_alt_1_outlined,color: Colors.white),
-                                    title: Text("Add new tenant",style: TextStyles(context).getRegularStyle().copyWith(color: Colors.white),),
-                                  ),
-                                  ListTile(
-                                    leading: const Icon(Icons.view_list_outlined,color: Colors.white),
-                                    title: Text("View Reports",style: TextStyles(context).getRegularStyle().copyWith(color: Colors.white),),
-                                  ),
-                                ],
-                              ),
-                                  Positioned(bottom:10,right:10,child: FloatingActionButton(onPressed: () {
-                                    Navigator.pop(context);
-                                  }, child: const Icon(Icons.close)))
 
-                            ],
+void showOptions(BuildContext context) {
+  showModalBottomSheet(
+      barrierColor: Colors.black.withOpacity(0.7),
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (context) {
+        return BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+          child: BottomSheet(
+              backgroundColor: Colors.transparent,
+              onClosing: () {},
+              builder: (context) {
+                return Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.transparent,
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          ListTile(
+                            onTap: () {
+                              Navigator.pop(context);
+                              Navigator.of(context).pushNamed(Routes.addTenant);
+                            },
+                            leading:const CircleAvatar(
+                              child:  Icon(Icons.person_add_alt_1_outlined,
+                                  color: Colors.white),
+                            ),
+                            title: Text(
+                              "Add new tenant",
+                              style: TextStyles(context)
+                                  .getRegularStyle()
+                                  .copyWith(color: Colors.white),
+                            ),
                           ),
-                        );
-                      }),
+                          ListTile(
+                            leading: const CircleAvatar(
+                              child: Icon(Icons.view_list_outlined,
+                                  color: Colors.white),
+                            ),
+                            title: Text(
+                              "View Reports",
+                              style: TextStyles(context)
+                                  .getRegularStyle()
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        right: 10,
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(Icons.close),
+                        ),
+                      ),
+                    ],
+                  ),
                 );
-              });
+              }),
+        );
+      });
 }
